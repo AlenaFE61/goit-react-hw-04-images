0c92dd472c678c107a8f546de695d783da217bb0
YukhymykAlena <o.yukhymyk@gmail.com>
2023-02-21 13:38:19 +0200

    hw4(3)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,101 @@
+import { useState, useEffect } from 'react';
+import { fetchImages } from './services/api';
+import { Searchbar } from './Searchbar/Searchbar';
+import { Loader } from './Loader/Loader';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+import { animateScroll } from 'react-scroll';
+import { Modal } from './Modal/Modal';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+  const [searchQuery, setSearchQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadMore, setLoadMore] = useState(false);
+  const [error, setError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const per_page = 12;
+
+  useEffect(() => {
+    getImages(searchQuery, page);
+  }, [searchQuery, page]);
+
+  const getImages = async (searchQuery, page) => {
+    if (!searchQuery) {
+      return;
+    }
+    setIsLoading(true);
+
+    try {
+      const { hits, totalHits } = await fetchImages(searchQuery, page);
+      if (hits.length === 0) {
+        return alert('Sorry, nothing found ');
+      }
+      console.log(hits, totalHits);
+      setImages(prevImages => [...prevImages, ...hits]);
+      setLoadMore(page < Math.ceil(totalHits / per_page));
+    } catch (error) {
+      setError({ error });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const formSubmit = searchQuery => {
+    setSearchQuery(searchQuery);
+    setImages([]);
+    setPage(1);
+    setLoadMore(false);
+  };
+
+  const onloadMore = () => {
+    setIsLoading(true);
+    setPage(prevPage => prevPage + 1);
+    scrollOnMoreButton();
+  };
+
+
+
+  const scrollOnMoreButton = () => {
+    animateScroll.scrollToBottom({
+      duration: 300,
+      delay: 10,
+      smooth: 'linear',
+    });
+  };
+
+  const openModal = largeImageURL => {
+    console.log(largeImageURL);
+    setShowModal(true);
+    setLargeImageURL(largeImageURL);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+    return (
+      <>
+      {isLoading &&<Loader />}
+
+        <Searchbar onSubmit={formSubmit} />
+
+        {images.length > 0 ? (
+          <ImageGallery images={images} openModal={openModal} />
+          ) : (<p></p>
+          )}
+        {error && <p>something went wrong</p>}
+
+        {loadMore && <Button onloadMore={onloadMore} page={page} />}
+        
+        {showModal && (
+          <Modal largeImageURL={largeImageURL} 
+          onClose={closeModal} />
+        )}
+     
+      </>
+    );
+}
+
